bb596bdb9edc7deee66cf8a1f0830aed465d551b
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-10-13 21:06:50 -0700

    Updated the app.js

--- Develop/app.js
+++ Develop/app.js
@@ -34,18 +34,6 @@ const render = require("./lib/htmlRenderer");
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
 
 const emp = [];
 
@@ -76,4 +64,45 @@ function addMember() {
     {
         message: "Enter team member's email address",
         name: "email"
-    }])}
+    }]).then(function ({ name, role, id, email }) {
+        let roleInfo = "";
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";
+        } else if (role === "Intern") {
+            roleInfo = "school name";
+        } else {
+            roleInfo = "office phone number";
+        }
+        inquirer.prompt([{
+            message: `Enter team member's ${roleInfo}`,
+            name: "roleInfo"
+        },
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: [
+                "yes",
+                "no"
+            ],
+            name: "moreMembers"
+        }]).then(function ({ roleInfo, moreMembers }) {
+            let newMember;
+            if (role === "Engineer") {
+                newMember = new Engineer(name, id, email, roleInfo);
+            } else if (role === "Intern") {
+                newMember = new Intern(name, id, email, roleInfo);
+             } else {
+               newMember = new Manager(name, id, email, roleInfo);
+                }
+            emp.push(newMember);
+            addHtml(newMember)
+            .then(function () {
+                if (moreMembers === "yes") {
+                    addMember();
+                 } else {
+                     finishHtml();
+                        }
+            });
+        });
+    });
+}
